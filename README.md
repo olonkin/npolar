30f4fc10638a5111304e46a7a9773a3068f2049a
Elias Sundby Aukan <esundbya@gmail.com>
2021-10-04 16:09:25 +0200

    Løsning på oppgave 1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # npolar
+
+kjør oppgave 1 (må ha python installert):
+
+> py np1.py
---
+++ np1.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import urllib.request, json
+
+class expedition:
+    def __init__(self, id, programs, first, last):
+        self.id = id
+        self.programs = programs
+        self.first_sample = first
+        self.last_sample = last
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=False, indent=None)
+
+url = "https://api.npolar.no/marine/biology/sample/?q=&fields=expedition,utc_date,programs,conveyance&limit=all&format=json&variant=array"
+response = urllib.request.urlopen(url).read()
+samples = json.loads(response.decode('UTF-8'))
+
+expeditions: list[expedition] = []
+
+for sample in samples:
+    existing = next((x for x in expeditions if x.id == sample['expedition']), None)
+    if existing is None:
+        expeditions.append(expedition(sample['expedition'], sample['programs'], sample['utc_date'], sample['utc_date']))
+    elif existing.last_sample < sample['utc_date']:
+        existing.last_sample = sample['utc_date']
+    elif existing.first_sample > sample['utc_date']:
+        existing.first_sample = sample['utc_date']
+
+for ex in expeditions:
+    print(ex.toJSON())
